5f0ec6db4a1cd2eb80c5534b49aaf07d513ce915
Lucas Siano <sianolucasdev@hotmail.com>
2023-06-06 19:02:36 -0300

    ejercicios resueltos

--- EjerciciosJava/src/main/java/org/ejerciciosJava/act4/MainCalcPropina.java
+++ EjerciciosJava/src/main/java/org/ejerciciosJava/act4/MainCalcPropina.java
@@ -1,12 +1,25 @@
 package org.ejerciciosJava.act4;
-/*      Calculadora de propinas:
-        Pídele al usuario que ingrese el total de la cuenta en un restaurante.
-        Pídele al usuario que ingrese el porcentaje de propina que desea dejar.
-        Calcula el monto de la propina utilizando la fórmula: propina = totalCuenta * (porcentajePropina / 100).*/
+/*
+      Calculadora de propinas:
+      Pídele al usuario que ingrese el total de la cuenta en un restaurante.
+      Pídele al usuario que ingrese el porcentaje de propina que desea dejar.
+      Calcula el monto de la propina utilizando la fórmula: propina = totalCuenta * (porcentajePropina / 100).
+*/
 
+
+import java.util.Scanner;
 
 public class MainCalcPropina {
     public static void main(String[] args) {
+
+        Scanner read = new Scanner(System.in);
+        System.out.println("Ingresa el total de la cuenta.");
+        Double totalCuenta = read.nextDouble();
+        System.out.println("Ingrese el porcentaje de propina que desea dejar.");
+        Double porcentajePropina = read.nextDouble();
+        Double Propina = totalCuenta *(porcentajePropina/100);
+
+        System.out.println("El monto de la propina es: " + Propina);
 
     }
 }
--- EjerciciosJava/src/main/java/org/ejerciciosJava/act5/MainAdivinNum.java
+++ EjerciciosJava/src/main/java/org/ejerciciosJava/act5/MainAdivinNum.java
@@ -1,13 +1,32 @@
 package org.ejerciciosJava.act5;
 
+import java.util.Scanner;
 
 /*       5) Juego de adivinar el número:
           Genera un número aleatorio entre 1 y 100. Pídele al usuario que adivine el número.*/
 public class MainAdivinNum {
     public static void main(String[] args) {
 
+        Scanner read = new Scanner(System.in);
+
+        int numAleatorio = (int) (Math.random() * 100) + 1;
+        System.out.println("Adivina el número..");
+        int numUser = read.nextInt();
+
+        if (numAleatorio == numUser) {
 
 
+        }
+
+        if (numUser > numAleatorio) {
+            System.out.println("El número es mayor que ese..");
+        } else if (numUser < numAleatorio) {
+            System.out.println("El número es menor que ese..");
+        } else if (numUser == numAleatorio) {
+            System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+            System.out.println("!!ACERTASTE¡¡ Ganaste un millón de dolares.");
+            System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        }
 
 
     }
