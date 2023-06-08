f914c3707e852a84d69592bcd2b5c61dea5d8263
Lucas Siano <sianolucasdev@hotmail.com>
2023-06-08 20:06:22 -0300

    test

---
+++ EjerciciosJava/src/main/java/org/ejerJava_clase6/Act1_googleMaps.java
@@ -0,0 +1,14 @@
+package org.ejerJava_clase6;
+
+/*
+Arma un buscador de países con Google Maps.
+Primero, pidele al usuario que ingrese un país (Ej: Colombia).
+Después muestra en consola la concatenación de:
+“https://www.google.com/maps/search/” + pais
+Entonces, cuando hagas click en el link que aparece en consola, te abrirá esa ubicación.
+*/
+public class Act1_googleMaps {
+
+
+
+}
